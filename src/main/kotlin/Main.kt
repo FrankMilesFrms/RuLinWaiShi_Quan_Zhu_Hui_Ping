a1c9d7ee093a45a9cac445389134a6693a70bd33
‘FrankMilesFrms’ <3505826836@qq.com>
2024-08-27 01:07:47 +0800

    beta 3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -40,7 +40,7 @@ fun main()
 		val testPath = "C:\\Users\\Frms\\Desktop\\RuLiWaiShi\\html\\chapter0${completionIndex(i)}.html"
 		val htmlLexer = HtmlLexer(File(testPath))
 
-		val savePath = "E:\\Projects\\IdeaProjects\\RuLiWaiShiHtmlToDocx\\out\\${htmlLexer.getTitle()}.docx"
+		val savePath = "E:\\Projects\\IdeaProjects\\RuLiWaiShiHtmlToDocx\\output_files\\${htmlLexer.getTitle()}.docx"
 
 		htmlLexer.bodyLexer { textList->
 			val m = MergeText()
